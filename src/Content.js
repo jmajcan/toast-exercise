d96efdc8c1ab436276b54245e6e4a33fa53c8ad9
Jake Majcan <jacob.majcan16@gmail.com>
2024-08-15 22:18:10 -0700

    Added error handling

--- src/Content.js
+++ src/Content.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
 
 import FormSnackbar from './components/FormSnackbar';
 import { fetchLikedFormSubmissions } from './service/mockServer';
@@ -10,30 +11,51 @@ import { useToast } from './contexts/ToastContext';
 
 export default function Content() {
   const [ submissionList, setSubmissionList ] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+
+  const [ isError, setIsError] = useState(false);
+
   const { hasSubmissionsChanged, setHasSubmissionsChanged } = useToast();
 
   useEffect(() => {
     if (hasSubmissionsChanged){
-      setIsLoading(true);
       fetchLikedFormSubmissions()
         .then((value) => {
-          console.log(value);
-          setSubmissionList(value.formSubmissions);
           setHasSubmissionsChanged(false);
-          setIsLoading(false);
+          setIsError(false);
+          setSubmissionList(value.formSubmissions);
+        })
+        .catch((err) => {
+          console.log(`ERROR ${err.status}: ${err.message}`);
+          setHasSubmissionsChanged(false);
+          setIsError(true);
         });
     }
   }, [hasSubmissionsChanged, setHasSubmissionsChanged, setSubmissionList]);
 
   const renderContent = () => {
-    if (!isLoading && submissionList.length > 0) {
+    if (isError) {
+      return (
+        <Box display="flex" sx={{flexDirection: 'row'}}>
+          <Typography variant="body1" sx={{fontWeight: 'bold', marginTop: 1, color: 'red'}}>
+          An error occured please try again.
+          </Typography>
+          <Button variant="contained" color="error" sx={{ marginLeft: 8 }} onClick={() => {
+            setHasSubmissionsChanged(true);
+            setIsError(false);
+          }}>
+            Try Again
+          </Button>
+        </Box>
+      );
+    }
+  
+    if (!hasSubmissionsChanged && submissionList.length > 0) {
       return submissionList.map(({data}, index) => (
         <Typography key={index} variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
           {`${data.firstName} ${data.lastName}: ${data.email}`}
         </Typography>
       ));
-    } else if (!isLoading && submissionList.length === 0) {
+    } else if (!hasSubmissionsChanged && submissionList.length === 0) {
       return(
         <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
           No Submissions Currently
@@ -45,9 +67,9 @@ export default function Content() {
   }
 
   return (
-    <Box sx={{marginTop: 3}}>
+    <Box sx={{marginTop: 3, justifyContent: 'center'}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
-        {isLoading ? (
+        {hasSubmissionsChanged ? (
           <Box sx={{marginTop: 2}}>
             <CircularProgress/>
           </Box>
